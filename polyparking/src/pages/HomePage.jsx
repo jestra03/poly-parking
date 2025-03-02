883f13073de310ebc2469bf321a499b220ea64d6
dragon123al <alin100@calpoly.edu>
2025-03-01 16:27:48 -0800

    added button hover, improved spacing / layout

--- polyparking/src/pages/HomePage.jsx
+++ polyparking/src/pages/HomePage.jsx
@@ -10,23 +10,29 @@ const HomePage = () => {
         <div className="page-container">
             <Header />
             <div className="content-container justify-center">
-                <div className="text-center mb-8">
-                    <p className="text-lg font-medium mb-6">Parking Spots Available: 27</p>
-                    <p className="text-lg font-medium mb-10">
-                        Estimated Time to School:<br />
-                        20 Minutes
+                <div className="text-center mb-4">
+                    <p className="text-lg font-medium mb-4">
+                        Available Parking:<br />
+                        27 Spots
                     </p>
 
                     <button
-                        className="button-secondary mb-4 w-48"
+                        className="button-secondary mb-6  mt-4 w-48 bg-blue-600 hover:bg-blue-900 text-white font-semibold py-2 px-4 rounded-lg transition duration-300"
                         onClick={() => navigate('/lots')}
                     >
                         Reserve A Spot
                     </button>
 
-                    <div className="mt-4">
+                    <p className="text-lg font-medium mb-6 mt-10">
+                        Estimated Time to School:<br />
+                        20 Minutes
+                    </p>
+
+                   
+
+                    <div className="mt-2">
                         <button
-                            className="button-secondary w-48"
+                            className="button-secondary mt-4 w-48 bg-blue-600 hover:bg-blue-900 text-white font-semibold py-2 px-4 rounded-lg transition duration-300"
                             onClick={() => navigate('/directions')}
                         >
                             Directions
