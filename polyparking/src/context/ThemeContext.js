190949488208870cd4fede1eb4511404791a6424
AnsonWooo <121963264+AnsonWooo@users.noreply.github.com>
2025-03-02 14:13:33 -0800

    finished with dark mode (#1)
    Co-authored-by: Joshua Estrada <112510910+jestra03@users.noreply.github.com>

---
+++ polyparking/src/context/ThemeContext.js
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useEffect, useState } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+    const [darkMode, setDarkMode] = useState(
+        () => localStorage.getItem('darkMode') === 'true' // load initial state from storage
+    );
+
+    useEffect(() => {
+        if (darkMode) {
+            document.documentElement.classList.add('dark');
+            document.body.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            document.body.classList.remove('dark');
+        }
+        localStorage.setItem('darkMode', darkMode);
+    }, [darkMode]);
+
+    return (
+        <ThemeContext.Provider value={{ darkMode, setDarkMode }}>
+            {children}
+        </ThemeContext.Provider>
+    );
+};
+
+export const useTheme = () => useContext(ThemeContext);
